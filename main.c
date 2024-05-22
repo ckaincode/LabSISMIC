a413ed6692a60ee05f6d12a2c41f527832e65e38
ckaincode <88911909+ckaincode@users.noreply.github.com>
2024-05-22 10:41:22 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -19,43 +19,25 @@ void config_chaves(void){
 }
 
 void config_timers(void){
-    TA0CTL = TASSEL__SMCLK | MC__UP | TAIE;
-    TA0CCTL1 = CCIE;
-    TA0CCTL2 = CCIE;
-
+    TA0CTL = TASSEL__SMCLK | MC__UP;
     TA0CCR0 = 8192;
-    TA0CCR1 = 2457;
-    TA0CCR2 = 5734;
-}
-
-void debounce(void){
-    volatile int i = 10000;
-    while (i--);
-}
-
-#pragma vector = TIMER0_A1_VECTOR
-__interrupt void led(void){
-    int seila = TA0IV;
-    if (seila == 2){
-        P1OUT &= ~BIT0;
-    }
-    if (seila == 4){
-        P4OUT &= ~BIT7;
-    }
-    if (seila == 14){
-        P1OUT |= BIT0;
-        P4OUT |= BIT7;
-    }
+    TA0CCR1 = 8192 / 32;
 }
 
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
     config_leds();
-    config_chaves();
     config_timers();
-    __enable_interrupt();
 
-    while (2>1);
+    while (2>1){
+        while (!(TA0CCTL1 & CCIFG));
+        TA0CCTL1 &= ~CCIFG;
+        P1OUT &= ~BIT0;
+
+        while (!(TA0CTL & TAIFG));
+        TA0CTL &= ~TAIFG;
+        P1OUT |= BIT0;
+    }
     return 0;
 }
