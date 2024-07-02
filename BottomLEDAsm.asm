d7754a3a9c284a8a14cb355093142f63d3ca15ea
ckaincode <88911909+ckaincode@users.noreply.github.com>
2024-07-02 18:28:16 -0300

    Update BottomLEDAsm.asm

--- BottomLEDAsm.asm
+++ BottomLEDAsm.asm
@@ -23,24 +23,7 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
-SW_LEDS:	call #CONFIG
-
-LB1:		bit.b #BIT1,&P2IN
-			jz LB2
-			bic.b #BIT0,&P1OUT
-			jmp LB3
-
-LB2:		bis.b #BIT0,&P1OUT
-
-LB3:		bit.b #BIT1, &P1IN
-			jz LB4
-			bic.b #BIT7, &P4OUT
-			jmp LB1
-
-LB4:		bis.b #BIT7,&P4OUT
-			jmp LB1
-
-CONFIG:		bis.b #BIT0, &P1DIR
+SW_LEDS:		bis.b #BIT0, &P1DIR
 			bic.b #BIT0, &P1OUT
 			bis.b #BIT7,&P4DIR
 			bic.b #BIT7,&P4OUT
@@ -50,8 +33,23 @@ CONFIG:		bis.b #BIT0, &P1DIR
 			bic.b #BIT1, &P1DIR
 			bis.b #BIT1,&P1REN
 			bis.b #BIT1,&P1OUT
-			jmp LB1
-			ret
+			call #START
+
+START:		bit.b #BIT1,&P2IN
+			jz REDSET
+			bic.b #BIT0,&P1OUT
+			jmp GREENTEST
+
+REDSET:		bis.b #BIT0,&P1OUT
+
+GREENTEST:		bit.b #BIT1, &P1IN
+			jz GREENSET
+			bic.b #BIT7, &P4OUT
+			jmp START
+
+GREENSET:		bis.b #BIT7,&P4OUT
+			jmp START
+
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
